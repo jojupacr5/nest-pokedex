27e79a07a4973bda2835daca46db621fa471fb43
jojupacr5 <joseph.paucar@gmail.com>
2024-11-15 23:18:30 -0500

    Se creo un Axios Adapter para separar la dependencia de axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
-import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
-import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ){}
   
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({}); // delete * from Pokemons
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=500');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     //* 1. Esta es una forma de inserción de registros simultaneos
     // const insertPromisesArray = [];
